4f17bd0b0109248f0fad2f1c03fed304ca9ae3fe
amir hemmateenejad <amirhemmateenejad@googlemail.com>
2024-02-19 00:51:01 +0330

    first commit

---
+++ go.mod
@@ -0,0 +1,29 @@
+module github.com/Amir-hn/vpn_download
+
+go 1.19
+
+require github.com/xxf098/lite-proxy v0.15.0
+
+require (
+	github.com/Dreamacro/go-shadowsocks2 v0.1.8 // indirect
+	github.com/gofrs/uuid v4.3.1+incompatible // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/miekg/dns v1.1.50 // indirect
+	github.com/v2fly/vmessping v0.3.4 // indirect
+	go.starlark.net v0.0.0-20190225160109-1174b2613e82 // indirect
+	go.uber.org/atomic v1.10.0 // indirect
+	golang.org/x/crypto v0.4.0 // indirect
+	golang.org/x/image v0.2.0 // indirect
+	golang.org/x/mod v0.7.0 // indirect
+	golang.org/x/net v0.4.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
+	golang.org/x/sys v0.3.0 // indirect
+	golang.org/x/text v0.5.0 // indirect
+	golang.org/x/tools v0.4.0 // indirect
+	google.golang.org/genproto v0.0.0-20221207170731-23e4bf6bdc37 // indirect
+	google.golang.org/grpc v1.51.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	v2ray.com/core v4.19.1+incompatible // indirect
+)
---
+++ src/main.go
@@ -0,0 +1,153 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	// "github.com/v2fly/vmessping"
+	"context"
+
+	"github.com/xxf098/lite-proxy/web"
+)
+
+func main() {
+	v2rayConfigFiles := "https://raw.githubusercontent.com/barry-far/V2ray-Configs/main/All_Configs_Sub.txt"
+
+	configFileName := "configs.txt"
+
+	if _, err := os.Stat(configFileName); err == nil {
+		fmt.Println("file exists")
+		errFile := os.Remove(configFileName)
+
+		if errFile != nil {
+			log.Fatal(errFile)
+			os.Exit(0)
+		}
+	}
+
+	out, err := os.Create(configFileName)
+
+	if err != nil {
+		log.Fatal(err.Error())
+		os.Exit(-1)
+	}
+
+	defer out.Close()
+
+	fmt.Println("create file")
+
+	resp, err := http.Get(v2rayConfigFiles)
+
+	if err != nil {
+		log.Fatal(err.Error())
+		os.Exit(-2)
+	}
+	defer resp.Body.Close()
+
+	fmt.Print("download from web:")
+	fmt.Println(resp.StatusCode)
+
+	if resp.StatusCode != 200 {
+		log.Fatal(err.Error())
+		os.Exit(-1)
+	}
+
+	_, err1 := io.Copy(out, resp.Body)
+
+	if err1 != nil {
+		log.Fatal(err.Error())
+		os.Exit(-2)
+	}
+
+	fmt.Println("download file and copy is complete")
+
+	file, err := os.OpenFile(configFileName, os.O_RDONLY, os.ModePerm)
+
+	if err != nil {
+		log.Fatal(err.Error())
+		os.Exit(-3)
+	}
+
+	// const maxCapacity int = 200
+	// buf := make([]byte, maxCapacity)
+
+	scanner := bufio.NewScanner(file)
+
+	// scanner.Buffer(buf, maxCapacity)
+	fmt.Println("start scan")
+
+	outputConfigs := make([]string, 0)
+
+	for scanner.Scan() {
+
+		line := scanner.Text()
+
+		_, err := url.ParseRequestURI(line)
+
+		if err != nil {
+			continue
+		}
+
+		ctx := context.Background()
+
+		opts := web.ProfileTestOptions{
+			GroupName:     "Default",
+			SpeedTestMode: "all",        //  pingonly speedonly all
+			PingMethod:    "googleping", // googleping
+			SortMethod:    "rspeed",     // speed rspeed ping rping
+			Concurrency:   50,
+			TestMode:      2,
+			Subscription:  line,
+			Language:      "en", // en cn
+			FontSize:      24,
+			Theme:         "rainbow",
+			Unique:        true,
+			Timeout:       5 * time.Second,
+			OutputMode:    0,
+		}
+
+		nodes, errLink := web.TestContext(ctx, opts, &web.EmptyMessageWriter{})
+		if errLink != nil {
+			fmt.Println("error in this line")
+			fmt.Println(errLink)
+		}
+
+		for _, node := range nodes {
+			ping, errPing := strconv.ParseInt(node.Ping, 10, 32)
+			if errPing != nil {
+				continue
+			}
+
+			if node.IsOk && ping > 0 && node.MaxSpeed > 0 {
+				outputConfigs = append(outputConfigs, node.Link)
+			}
+		}
+
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+		os.Exit(-4)
+	}
+
+	f, errFile := os.Create("./valid.txt")
+
+	if errFile != nil {
+		fmt.Println("error in write file")
+		fmt.Println(errFile)
+	}
+
+	f.WriteString(strings.Join(outputConfigs[:], "\n"))
+	f.Sync()
+
+	defer f.Close()
+
+}
